d32fc6d434a51d994311ec373b1eb432b396434e
엄수혁 <soohyuk96@naver.com>
2023-06-19 18:43:05 +0900

    FIX : [Greedy] 백준 20300 풀이

--- 엄수혁/탐욕법(Greedy)/BOJ_20300_서강근육맨.java
+++ 엄수혁/탐욕법(Greedy)/BOJ_20300_서강근육맨.java
@@ -6,21 +6,39 @@ import java.util.Arrays;
 import java.util.List;
 
 /*
-* 1. 문제 요약
-* 주어진 N자리 수열에서 숫자 K개를 지워서 얻을 수 있는 가장 큰 수
-*
-* 2. 아이디어
-* 데크를 통해 문자 비교열을 해나가며 k--를 통해 현재 지워진 숫자를 확인
-* 이 과정 중 못 지워져서 K가 남은 경우
-* 그 만큼 데크의 마지막 부분에서 요소를 삭제한다.
-* */
-public class B_20300{
+ * 1. 문제 요약
+ * 가장 근손실이 큰 날을 찾기
+ *
+ * 2. 아이디어
+ * 홀수인 경우만 맥스값을 지정해주고, 나머지는 짝수와 동일 로직
+ *
+ * */
+public class B_20300 {
     public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(in.readLine());
-        long[] array = Arrays.stream(in.readLine().split(" ")).mapToLong(Long::parseLong).sorted().toArray();
+       import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
-        System.out.println(array.length % 2 == 1 ? array[array.length-1] : array[array.length-1] + array[0]);
-        List<Integer> list = new ArrayList<>(1);
+        public class Main {
+            public static void main(String[] args) throws IOException {
+                BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+                int N = Integer.parseInt(in.readLine());
+                long[] intArr = Arrays.stream(in.readLine().split(" ")).mapToLong(Long::parseLong).sorted().toArray();
+                long max = Long.MIN_VALUE;
+
+                if (N % 2 != 0) {
+                    max = intArr[N - 1];
+                    for (int i = 0; i < N / 2; i++) {
+                        max = Math.max(intArr[i] + intArr[N - 2 - i], max);
+                    }
+                } else {
+                    for (int i = 0; i < N / 2; i++) {
+                        max = Math.max((intArr[i] + intArr[N - 1 - i]), max);
+                    }
+                }
+                System.out.println(max);
+            }
+        }
     }
 }
