608b4c3ba8224ccc7300b2644091c9b7bf382cd5
sanghyuk2 <givemesomefoodplz@gmail.com>
2023-08-23 23:17:38 +0900

    ADD : [DFS&BFS] 백준 2606번 풀이

---
+++ 이상혁/깊이 & 너비 우선 탐색 (DFS & BFS)/BOJ_2606_바이러스.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int answer = 0;
+    static boolean[] visited;
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        visited = new boolean[N + 1];
+
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+
+        }
+
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            graph.get(A).add(B);
+            graph.get(B).add(A);
+        }
+
+        bfs(1);
+        System.out.println(answer);
+
+    }
+
+    private static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+
+            for (int next : graph.get(now)) {
+                if (!visited[next]) {
+                    queue.offer(next);
+                    visited[next] = true;
+                    answer++;
+                }
+            }
+        }
+    }
+}
